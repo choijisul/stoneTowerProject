a48d62618b66749d1dae190c244543e713874e7d
user.email.w2217@e-mirim.hs.kr <user.email.w2217@e-mirim.hs.kr>
2023-11-10 10:47:49 +0900

    stone 반응형

--- js/buttonClickStone.js
+++ js/buttonClickStone.js
@@ -5,10 +5,25 @@ var period = storedSelectedValue;
 var count = 0;
 
 // 이미지 위치 정보
-var imgWidth = 25.0;
+var imgWidth = 0;
 var imgHeight = 'auto';
-var imgLeft = 21;
-var imgTop = 570;
+var imgLeft = 0;
+var imgTop = 0;
+
+if (window.innerWidth <= 600) {  //테블릿
+    imgWidth = 60.0;
+    imgLeft = 20;
+    imgTop = 570;
+} else if (window.innerWidth <= 1000) {
+    imgWidth = 30.0;
+    imgLeft = 14;
+    imgTop = 570;
+} else {
+    imgWidth = 25.0;
+    imgLeft = 21;
+    imgTop = 570;
+}
+
 
 // 이미지 URL
 var imageUrls = ['./img/stone1.png', './img/stone2.png', './img/stone3.png'];
@@ -24,16 +39,18 @@ button.addEventListener('click', function () {
         img.src = imageUrl;
 
         // 이미지 너비가 0보다 클 때만 이미지 너비 조절
-        if (imgWidth > 0) {
-            if (imgWidth <= 8) {
-                imgWidth -= 1.5;
-                imgLeft += 1;
-                imgTop -= 30;
-            } else {
-                imgWidth -= 1.5;
-                imgLeft += 1;
-                imgTop -= 40;
-            }
+        if (window.innerWidth <= 600) {  //테블릿
+            imgWidth -= 5;
+            imgLeft += 3;
+            imgTop -= 30;
+        } else if (window.innerWidth <= 1000) {
+            imgWidth -= 2;
+            imgLeft += 1;
+            imgTop -= 30;
+        } else {
+            imgWidth -= 2;
+            imgLeft += 1;
+            imgTop -= 30;
         }
 
         img.style.width = imgWidth + '%';
@@ -47,10 +64,10 @@ button.addEventListener('click', function () {
         document.body.appendChild(img);
         count++;
 
-        if(count == period ){
-            setTimeout(function() {
+        if (count == period) {
+            setTimeout(function () {
                 alert("목표를 달성했습니다!!");
-            }, 1500);  
+            }, 1500);
         }
     }
 });
