d842c7af6ad12ffca11ac0fc22847f85390f74e8
user.email.w2217@e-mirim.hs.kr <user.email.w2217@e-mirim.hs.kr>
2023-11-11 11:23:06 +0900

    stoneTower, weekly

--- js/buttonClickStone.js
+++ js/buttonClickStone.js
@@ -8,22 +8,31 @@ var count = 0;
 var imgWidth = 0;
 var imgHeight = 'auto';
 var imgLeft = 0;
-var imgTop = 0;
+var imgTop = 480;
 
-if (window.innerWidth <= 600) {  //테블릿
+var stoneTowerImgWidth = 5;
+var stoneTowerImgHeight = 'auto';
+var stoneTowerImgLeft = 20;
+var stoneTowerImgTop = 600;
+
+if (window.innerWidth <= 600) {  // 테블릿
     imgWidth = 60.0;
     imgLeft = 20;
-    imgTop = 570;
+    imgTop = 460;
+    stoneTowerImgWidth = 15;
+    stoneTowerImgLeft = 16;
+    stoneTowerImgTop = 565;
 } else if (window.innerWidth <= 1000) {
     imgWidth = 30.0;
-    imgLeft = 14;
-    imgTop = 570;
+    imgLeft = 10;
+    imgTop = 490;
+    stoneTowerImgWidth = 7;
+    stoneTowerImgLeft = 10;
+    stoneTowerImgTop = 590;
 } else {
     imgWidth = 25.0;
     imgLeft = 21;
-    imgTop = 570;
 }
-
 
 // 이미지 URL
 var imageUrls = ['./img/stone1.png', './img/stone2.png', './img/stone3.png'];
@@ -37,9 +46,10 @@ button.addEventListener('click', function () {
         // 이미지 생성
         var img = document.createElement('img');
         img.src = imageUrl;
+        img.id = 'stoneImg'; // id 추가
 
         // 이미지 너비가 0보다 클 때만 이미지 너비 조절
-        if (window.innerWidth <= 600) {  //테블릿
+        if (window.innerWidth <= 600) {  // 테블릿
             imgWidth -= 5;
             imgLeft += 3;
             imgTop -= 30;
@@ -69,5 +79,48 @@ button.addEventListener('click', function () {
                 alert("목표를 달성했습니다!!");
             }, 1500);
         }
+
+        if (count % 7 == 0) {
+            var stonetowerImg = document.createElement('img');
+
+            // 지금까지 js로 새로 생성한 이미지 모두 없애기
+            var allImages = document.querySelectorAll('#stoneImg'); // 'stoneImg' id를 가진 이미지 엘리먼트들을 모두 가져옴
+
+            allImages.forEach(function (image) {
+                image.remove(); // 각 'stoneImg' id를 가진 이미지를 제거
+            });
+
+            imgTop += 210;
+            if (window.innerWidth <= 600) {  // 테블릿
+                imgWidth = 60.0;
+                imgLeft = 20;
+            } else if (window.innerWidth <= 1000) {
+                imgWidth = 30.0;
+                imgLeft = 14;
+            } else {
+                imgWidth = 25.0;
+                imgLeft = 21;
+            }
+
+            // 이미지
+            stonetowerImg.src = './img/icon.png';
+            stonetowerImg.style.width = stoneTowerImgWidth + '%';
+            stonetowerImg.style.height = stoneTowerImgHeight;
+
+            stonetowerImg.style.position = 'absolute';
+            stonetowerImg.style.left = stoneTowerImgLeft + "%";
+            stonetowerImg.style.top = stoneTowerImgTop + "px";
+
+            if (window.innerWidth <= 600) {  // 테블릿
+                stoneTowerImgLeft += 15;
+            } else if (window.innerWidth <= 1000) {
+                stoneTowerImgLeft += 7;
+            } else {
+                stoneTowerImgLeft += 5;
+            }
+            document.body.appendChild(stonetowerImg);
+
+        }
+
     }
 });
